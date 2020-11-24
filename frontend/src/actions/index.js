03b3c620752fd1507c291fdcf5faa68dc456c320
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-23 23:28:34 -0500

    Add getFavorites to get data from db

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,5 +1,13 @@
 export const ADD_DEFINITION = 'ADD_DEFINITION'
 export const REMOVE_FROM_FAVORITES = 'REMOVE_FROM_FAVORITES'
+export const GET_FAVORITES = 'GET_FAVORITES'
+
+export function getFavorites (data) {
+  return {
+    type: GET_FAVORITES,
+    data
+  }
+}
 
 export function addDefinition ({ definition, word }) {
   return {
