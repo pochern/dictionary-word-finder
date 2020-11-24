6a7afdada2410520d08d451f4b16dac3d346a1b7
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-23 20:32:07 -0500

    Filter favorites based on type of word

--- src/components/ViewFavorites.js
+++ src/components/ViewFavorites.js
@@ -1,23 +1,67 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Typography from '@material-ui/core/Typography'
 import { removeFromFavorites } from '../actions'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
+import MenuItem from '@material-ui/core/MenuItem'
+import Select from '@material-ui/core/Select'
+import Typography from '@material-ui/core/Typography'
 import WordList from './WordList'
 
 function ViewFavorites () {
   const definitions = useSelector(state => state.definitions)
-  console.log('THESE ARE THEM', definitions)
+  const [filteredDefinitions, setFilteredDefinitions] = useState(definitions)
+  const [type, setType] = useState('')
   const dispatch = useDispatch()
+
+  // Applies filter after definition is removed
+  useEffect(() => {
+    applyFilter()
+  }, [definitions])
+
+  // Applies filter after type is selected
+  useEffect(() => {
+    applyFilter()
+  }, [type])
+
+  const applyFilter = () => {
+    if (type !== '') {
+      setFilteredDefinitions(definitions.filter((elem) => type === elem.definition.type))
+    } else {
+      setFilteredDefinitions(definitions)
+    }
+  }
+
+  const handleChange = (e) => {
+    setType(e.target.value)
+  }
+
+  const types = ['noun', 'verb', 'adjective', 'adverb', 'pronoun', 'preposition', 'conjunction', 'interjection', 'numeral', 'article', 'determiner']
 
   return (
     <div>
       <div>
+        <FormControl variant="outlined" style={{ minWidth: '5em', marginBottom: '2em' }}>
+          <InputLabel>Type</InputLabel>
+          <Select
+            value={type}
+            onChange={handleChange}
+            label="Type"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            {types.map((elem, index) => (
+              <MenuItem key={index} value={elem}>{elem}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <Typography variant='h6' style={{ marginBottom: '2em' }}>
           View Favorites
         </Typography>
         <WordList
-          definitions={definitions}
+          definitions={filteredDefinitions}
           onRemove={(definition, word) => {
             dispatch(removeFromFavorites({ definition, word }))
           }}
